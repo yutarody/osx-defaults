2c2f746c78615959ae82b32c697d207bba10fd7b
Yutaro Horikawa <yutarody@users.noreply.github.com>
2016-04-20 16:54:07 +0900

    Error Stop & kill terminal remove

--- defaults-write.sh
+++ defaults-write.sh
@@ -5,6 +5,8 @@
 # Ask for the administrator password upfront
 sudo -v
 
+set -e
+
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
@@ -226,7 +228,7 @@ defaults write com.apple.DiskUtility advanced-image-options -bool true
 ###############################################################################
 
 for app in "Dashboard" "Dock" "Finder" \
-    "Safari" "SystemUIServer" "Terminal" \
+    "Safari" "SystemUIServer" \
     "iTunes"; do
     killall "$app" > /dev/null 2>&1
 done
